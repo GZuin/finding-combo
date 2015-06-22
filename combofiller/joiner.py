15f6362ffcbdc86f2ac23926c52c9366d2e661f6
GZuin <gianzuin@gmail.com>
2015-06-22 12:34:23 -0300

    Script to join multiple files in one big file for training

---
+++ combofiller/joiner.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+
+endinputs = sys.argv[1]
+outfile = sys.argv[2]
+
+output = open(outfile, 'w')
+
+for i in range (0, int(endinputs)):
+    inputfile = str(i) + ".txt"
+    with open(inputfile) as f:
+        for char in f:
+            if(char != '\n'):
+                output.write(char)
+output.close()
+           
